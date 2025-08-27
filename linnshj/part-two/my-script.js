05294e74fecebdb7fced67f4f46e900ddee5cd28
linn-s-h <linnshjulstad@gmail.com>
2025-08-27 15:35:22 +0200

    start step 2: delete

--- linnshj/part-two/my-script.js
+++ linnshj/part-two/my-script.js
@@ -2,10 +2,29 @@
 function addCurrency(currencyName) {
     if (currencyName.trim() === "") return;
 
+    //Our Currency List
     const list = document.getElementById("currencyList");
+
+    //Create list item
     const li = document.createElement("li");
     li.textContent = currencyName;
+
+    //Create delete button
+    const deleteButton = document.createElement("button"); //Create type button
+    deleteButton.innerHTML = "&#10005;"; //Cross symbol
+    deleteButton.classList.add("deleteButton"); //Add class to button
+
+    //Remove li, when delete button is clicked
+    deleteButton.addEventListener("click", function(){
+        list.removeChild(li);
+    });
+
+    //Append li to list
     list.appendChild(li);
+
+    //Append delete button to li
+    li.appendChild(deleteButton);
+
 }
 
 document.getElementById("addButton").addEventListener("click", function() {
