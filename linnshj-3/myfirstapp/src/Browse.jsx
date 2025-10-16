9e8126693fc5f5693c8270cf9638b735376db333
linn-s-h <linnshjulstad@gmail.com>
2025-10-16 15:28:29 +0200

    finish Insert

--- linnshj-3/myfirstapp/src/Browse.jsx
+++ linnshj-3/myfirstapp/src/Browse.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDataQuery } from '@dhis2/app-runtime';
-import {Menu, MenuItem, Table, TableHead, TableBody, TableRow, TableCell} from "@dhis2/ui"
+import {Table, TableHead, TableBody, TableRow, TableCell} from "@dhis2/ui"
 
 const dataQuery = {
   dataSets: {
@@ -23,29 +23,12 @@ const dataQuery = {
   },
 }
 
+function mergeData(data) {
 
-export function Browse() {
-
-  const { loading, error, data } = useDataQuery(dataQuery)
-
-  if (error) {
-    return <span>ERROR: {error.message}</span>
-}
-
-if (loading) {
-    return <span>Loading...</span>
-}
-
-if (data) {
-
-  console.log("API response:",data)
-
-  //Get dataset elements (structure)
   const dataSetsItems = data?.dataSets?.dataSetElements || []
-  //Get recorded values (dataValues)
   const dataValueSetsItems = data?.dataValueSets?.dataValues || []
 
-   //Map elements to their values 
+  //Map elements to their values 
   const items = dataSetsItems.map((item) => {
     const match = dataValueSetsItems.find(
       (v) => v.dataElement === item.dataElement.id
@@ -53,34 +36,50 @@ if (data) {
     return {
       displayName: item.dataElement.displayName,
       id: item.dataElement.id,
-      value: match ? match.value : "—"
+      value: match.value
     }
   })
-
-  return (
-    <div style={{display: "flex", gap: "16px"}}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell><strong>Display Name</strong></TableCell>
-            <TableCell><strong>Value</strong></TableCell>
-            <TableCell><strong>ID</strong></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>{item.displayName}</TableCell>
-              <TableCell>{item.value}</TableCell>
-              <TableCell>{item.id}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      
-     
-    </div>
-  );
+  return items
 }
+
+
+export function Browse() {
+
+  const { loading, error, data } = useDataQuery(dataQuery)
+
+  if (error) {
+    return <span>ERROR: {error.message}</span>
+  }
+
+  if (loading) {
+      return <span>Loading...</span>
+  }
+
+  if (data) {
+    let mergedData = mergeData(data)
+    console.log("API response:",mergedData)
+    return (
+      <div style={{display: "flex", gap: "16px"}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><strong>Display Name</strong></TableCell>
+              <TableCell><strong>Value</strong></TableCell>
+              <TableCell><strong>ID</strong></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {mergedData.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.displayName}</TableCell>
+                <TableCell>{item.value}</TableCell>
+                <TableCell>{item.id}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
 }
 
--- linnshj-3/myfirstapp/src/Insert.jsx
+++ linnshj-3/myfirstapp/src/Insert.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import React from 'react'
+import {
+    ReactFinalForm,
+    InputFieldFF,
+    Button,
+    SingleSelectFieldFF,
+    hasValue,
+    number,
+    composeValidators,
+} from '@dhis2/ui'
 
-export function Insert() {
-  return <h1>Insert</h1>;
+export function Insert(props) {
+  function onSubmit(formInput) {
+      console.log(formInput)
+  }
+  return (
+    <div>
+      <ReactFinalForm.Form onSubmit={onSubmit}>
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <ReactFinalForm.Field
+              component={SingleSelectFieldFF}
+              name="dataElement"
+              label="Select field"
+              initialValue="WUg3MYWQ7pt"
+              options={[
+                {
+                  label: 'Total Population',
+                  value: 'WUg3MYWQ7pt',
+                },
+                {
+                  label: 'Population of women of child bearing age (WRA)',
+                  value: 'vg6pdjObxsm',
+                },
+                {
+                  label: 'Total population < 5 years  ',
+                  value: 'DTtCy7Nx5jH',
+                },
+                {
+                  label: 'Expected pregnancies',
+                  value: 'h0xKKjijTdI',
+                },
+                {
+                  label: 'Total population < 1 year   ',
+                  value: 'DTVRnCGamkV',
+                },
+              ]}
+            />
+            <ReactFinalForm.Field
+                name="value"
+                label="Value"
+                component={InputFieldFF}
+                validate={composeValidators(hasValue, number)}
+            />
+            <Button type="submit" primary>
+                Submit
+            </Button>
+          </form>
+        )}
+      </ReactFinalForm.Form>
+    </div>
+  )
 }
